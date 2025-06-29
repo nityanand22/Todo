c51d0265ae93e46efed6c26b4c98adbe2a7c5411
Nityanand <127835667+nityanand22@users.noreply.github.com>
2025-06-29 06:02:05 +0000

    project completed

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,66 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface Todo {
+  title: string;
+  isCompleted: boolean;
+  readonly id: string
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const todos: Todo[] = []
+const todosContainer = document.querySelector(".todoContainer") as HTMLDivElement
+const todoInput = document.querySelector("input") as HTMLInputElement;
+const myform = document.querySelector("#myform") as HTMLFormElement;
+
+myform.onsubmit = (e) => {
+  e.preventDefault();
+
+  const todo: Todo = {
+    title: todoInput.value,
+    isCompleted: false,
+    id: String(new Date(Date.now()))
+  }
+  todos.push(todo)
+  todoInput.value = ""
+  renderTodo(todos)
+}
+
+const generateTodoItem = (title: string, isCompleted: boolean, id: string) => {
+  const todo = document.createElement("div")
+  todo.className = "todo";
+  const checkBox: HTMLInputElement = document.createElement("input")
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.className = "isCompleted"
+  checkBox.checked = isCompleted
+
+  checkBox.onchange = () => {
+    todos.find(item => {
+      if (item.id === id) item.isCompleted = checkBox.checked
+    })
+    para.className = checkBox.checked ? "textCut" : ""
+  }
+
+
+  const para: HTMLParagraphElement = document.createElement("p")
+  para.innerText = title
+  para.className = isCompleted ? "textCut" : ""
+
+  const btn: HTMLButtonElement = document.createElement("button")
+  btn.innerText = "X"
+  btn.className = "deleteBtn"
+  btn.onclick = () => {
+    deleteTodo(id)
+    renderTodo(todos)
+  }
+  todo.append(checkBox, para, btn)
+  todosContainer.append(todo)
+}
+
+const deleteTodo = (id: string) => {
+  const idx = todos.findIndex(item => item.id === id)
+  todos.splice(idx, 1)
+}
+
+const renderTodo = (arr: Todo[]) => {
+  todosContainer.innerText = ""
+  arr.forEach(item => (generateTodoItem(item.title, item.isCompleted, item.id)))
+}
